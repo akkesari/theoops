878c2213b915de2cfaa8780a240909aa51d1febd
Paulo <pacarvalho@wpi.edu>
2014-11-11 12:12:34 -0500

    Partially added soccer torunament. Made change to MatchData to reflect new data type for contestant

--- Homework4/src/Examples.java
+++ Homework4/src/Examples.java
@@ -3,7 +3,32 @@ class Examples {
 	Examples(){
 		
 	}
+	/******************** Create Soccer Tournament **********************/
 	
-	// Create examples of Soccer 
+	// SAMn: Soccer Advanced Match n; SIMn: Soccer Init Match n
+	//			      sIM1
+	//			    /
+	//		   sAM1 - sIM2
+	//	sAM3 <
+	//		   sAM2 - sIM3
+	//			    \
+	//			      sIM4
+	
+	// Create examples of Soccer Teams
+	Soccer soccerTeam1 = new Soccer("The Loosers", 11, "The Lost One", 1);
+	Soccer soccerTeam2 = new Soccer("The Squirrels", 15, "The Candid One", 2);
+	Soccer soccerTeam3 = new Soccer("The Looms", 16, "The Ignored One", 3);
+	Soccer soccerTeam4 = new Soccer("The Winners", 13, "The Other One", 4);
+	
+	// Create Soccer Matches
+	SoccerScore soccerMatch1 = new SoccerScore(1, 2, true);
+	SoccerScore soccerMatch2 = new SoccerScore(2, 1, false);
+	SoccerScore soccerMatch3 = new SoccerScore(2, 3, true);
+	SoccerScore soccerMatch4 = new SoccerScore(5, 2, true);
+	SoccerScore soccerMatch5 = new SoccerScore(6, 1, false);
+	SoccerScore soccerMatch6 = new SoccerScore(7, 3, true);
+	SoccerMatch soccerMatch7 = new SoccerScore(1, 1, true);
+	
+	InitMatch sIM1 = new InitMatch()
 	
 }
--- Homework4/src/MatchData.java
+++ Homework4/src/MatchData.java
@@ -1,12 +1,12 @@
 // Keeps track of the data for a match
 class MatchData {
-	String contestant1;
-	String contestant2;
+	IContestant contestant1;
+	IContestant contestant2;
 	IScore score;
 	Venue matchVenue;
 	
 	// Constructor
-	MatchData(String contestant1, String contestant2, IScore score, Venue matchVenue){
+	MatchData(IContestant contestant1, IContestant contestant2, IScore score, Venue matchVenue){
 		this.contestant1 = contestant1;
 		this.contestant2 = contestant2;
 		this.score = score;
